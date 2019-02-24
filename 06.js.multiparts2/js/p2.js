5fc4ba5b3fb747f53a10e3b30ca567d0b36cfd26
chumpon-asaneerat <chumponsenate@gmail.com>
2019-02-24 11:19:22 +0700

    Add js multi part part 2

---
+++ 06.js.multiparts2/js/p2.js
@@ -0,0 +1,4 @@
+console.log('part 2 loaded..');
+window.nlib = {
+    showme: function() { console.log('hello!!'); }
+};
---
+++ 06.js.multiparts2/js/p3.js
@@ -0,0 +1,5 @@
+console.log('part 3 loaded..');
+let p2 = document.querySelector('#part2');
+//console.log('parent:', p2.parentNode);
+p2.parentNode.removeChild(p2);
+console.log('part 2 removed..');
