0f555cb5520aee0bf3beaf074f16708436d7c4a1
chumpon-asaneerat <chumponsenate@gmail.com>
2019-04-08 09:13:10 +0700

    Designer UI - add mouse handler code for select object(s) on designer div.

--- 04.WebUI2/04.DesignerUI/js/designer.js
+++ 04.WebUI2/04.DesignerUI/js/designer.js
@@ -1,3 +1,169 @@
-;(function(){
-    console.log('designer start...');
-})()
+
+class NDesigner {
+    constructor(elem) {
+        this._elem = elem;
+        this._objs = [];
+
+        this._selectedObj = null;
+
+        let self = this;
+        this._elem.addEventListener('mousedown', (e) => {
+            self.onMouseDown(e);
+        })
+        this._elem.addEventListener('mousemove', (e) => {
+            self.onMouseMove(e);
+        })
+        this._elem.addEventListener('mouseup', (e) => {
+            self.onMouseUp(e);
+        })
+    };
+    get element() { return this._elem; }
+    get objects() { return this._objs; }
+
+    onMouseDown(e) {
+        let el, evt = e ? e:event;
+        if (evt.srcElement)  el = evt.srcElement;
+        else if (evt.target) el = evt.target;
+
+        console.log(`designer mouse down:`, el);
+    };
+    onMouseMove(e) {
+        //console.log(`mouse move:`);
+    };
+    onMouseUp(e) {
+        let el, evt = e ? e:event;
+        if (evt.srcElement)  el = evt.srcElement;
+        else if (evt.target) el = evt.target;
+
+        console.log(`designer mouse up:`, el);
+    };
+
+    add(dsgnObj) {
+        if (!dsgnObj) return;
+        dsgnObj.designer = this;
+        this._elem.appendChild(dsgnObj.element);
+        this._objs.push(dsgnObj);
+    };
+    remove(dsgnObj) {
+        if (!dsgnObj) return;
+        dsgnObj.designer = null;
+    }
+
+    indexOf(dsgnObj) {
+        if (!dsgnObj) return -1;
+        return -1;
+    }
+
+    get selectedObj() { return this._selectedObj; }
+    set selectedObj(value) {
+        if (this._selectedObj !== value) {
+            console.log('new object selelected. id:', value.element.id);
+
+            this._objs.forEach(obj => {
+                if (obj !== value) obj.deselect();
+            });
+
+            this._selectedObj = value;
+        }
+    }
+}
+
+class NDesignObject {
+    constructor() {
+        this._designer = null;
+        this._elem = document.createElement('div');
+        this._elem.classList.add('design-object');
+        let self = this;
+        this._elem.addEventListener('mousedown', (e) => {
+            self.onMouseDown(e);
+        })
+        this._elem.addEventListener('mousemove', (e) => {
+            self.onMouseMove(e);
+        })
+        this._elem.addEventListener('mouseup', (e) => {
+            self.onMouseUp(e);
+        })
+    };
+
+    onMouseDown(e) {
+        if (!this.selected) {
+            console.log(`mouse down:`);
+            this.select();
+            e.preventDefault();
+            e.stopPropagation();
+            return false;
+        }
+    };
+    onMouseMove(e) {
+        if (!this.selected) return;
+        //console.log(`mouse move:`);
+    };
+    onMouseUp(e) {
+        if (!this.selected) return;
+        console.log(`mouse up:`);
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+    };
+
+    createHotspot(type, direction) {
+        let hotspot = document.createElement('div');
+        hotspot.classList.add('hotspot')
+        hotspot.setAttribute('type', type);
+        hotspot.setAttribute('direction', direction);
+        return hotspot;
+    };
+
+    get designer() { return this._designer; }
+    set designer(value) { this._designer = value; }
+
+    get element() { return this._elem; }
+    get selected() { return this._elem.hasAttribute('selected'); }
+
+    select() {
+        if (!this._elem.hasAttribute('selected')) {
+            // object is not selected so add selected attribute.
+            this._elem.setAttribute('selected', '');
+            // add hot spot child node.
+            this._elem.appendChild(this.createHotspot('resize', 'n'));
+            this._elem.appendChild(this.createHotspot('resize', 'e'));
+            this._elem.appendChild(this.createHotspot('resize', 'ne'));
+            this._elem.appendChild(this.createHotspot('resize', 'w'));
+            this._elem.appendChild(this.createHotspot('resize', 'nw'));
+            this._elem.appendChild(this.createHotspot('resize', 's'));
+            this._elem.appendChild(this.createHotspot('resize', 'se'));
+            this._elem.appendChild(this.createHotspot('resize', 'sw'));
+            // set selected object.
+            if (!this.designer) return;
+            this.designer.selectedObj = this;
+        }
+    };
+    deselect() {
+        if (this._elem.hasAttribute('selected')) {
+            // object is selected so remove selected attribute.
+            this._elem.removeAttribute('selected');
+            // remove hot spot child node.
+            let hotspots = this._elem.getElementsByClassName('hotspot');
+            for (let i = hotspots.length - 1; i >= 0; --i) {
+                let hotspot = hotspots[i];
+                hotspot.remove();
+            }
+        }
+    };
+}
+
+;(function() {
+    console.log('designer init...');
+    let dsgner = new NDesigner(document.getElementById('dsgn1'));
+    console.log(dsgner);
+
+    let obj1 = new NDesignObject();
+    obj1.element.id = 'obj1';
+    dsgner.add(obj1);
+
+    let obj2 = new NDesignObject();
+    obj2.element.id = 'obj2';
+    dsgner.add(obj2);
+
+    obj1.select();
+})();
