c8860c0cf167d426476318a24e53dee186392012
chumpon-asaneerat <chumponsenate@gmail.com>
2019-05-17 11:29:50 +0700

    Add sample Replace class method.

--- 01.Quokka/06.mixins/04.mixis.sample04.js
+++ 01.Quokka/06.mixins/04.mixis.sample04.js
@@ -34,6 +34,7 @@ console.log(new z());
 
 // mixin 02: Using Object.assign
 // Extend class method via Object.assign.
+/*
 class Car {
     constructor(name) {
         this._name = name;
@@ -62,3 +63,41 @@ const Brake = function() {
 Object.assign(Car.prototype, { Brake });
 myCar.Brake();
 console.log(myCar.status);
+*/
+
+// mixin 03: Replace class method.
+// Replace class method.
+// Note. this method cannot used with property.
+class Car {
+    constructor(name) {
+        this._name = name;
+        this._running = false;
+    }
+    status() { 
+        let status = (this._running) ? 'running' : 'stop'
+        let result = `${this._name} is ${status}.`;
+        return result;
+    }
+
+    get name() { return this._name; }
+    get start() { return this._running; }
+    set start(value) { this._running = value; }
+}
+
+let myCar = new Car('my toyota');
+console.log(myCar.status());
+myCar.start = true;
+console.log(myCar.status());
+
+const mystatus = function() {
+    console.log('new function');
+    let status = (this._running) ? 'still running' : 'full stop'
+    let result = `${this._name} car is ${status}.`;
+    return result;
+}
+
+myCar.status = mystatus;
+// or
+//myCar['status'] = mystatus;
+
+console.log(myCar.status());
