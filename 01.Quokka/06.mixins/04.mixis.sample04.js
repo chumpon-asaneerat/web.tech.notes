e85700d331025be90d010ffc650f6bc6c4c25198
chumpon-asaneerat <chumponsenate@gmail.com>
2019-05-17 11:07:05 +0700

    Add sample Extend class method via Object.assign

--- 01.Quokka/06.mixins/04.mixis.sample04.js
+++ 01.Quokka/06.mixins/04.mixis.sample04.js
@@ -1,3 +1,5 @@
+// mixin 01: check class name.
+/*
 class Hello {} // ES6 class declaration
 
 function Hello2() {} // Plain js class declaration
@@ -28,3 +30,35 @@ console.log(strI);
 let z = classes['Hello'];
 console.log(z);
 console.log(new z());
+*/
+
+// mixin 02: Using Object.assign
+// Extend class method via Object.assign.
+class Car {
+    constructor(name) {
+        this._name = name;
+        this._running = false;
+    }
+    get status() { 
+        let status = (this._running) ? 'running' : 'stop'
+        let result = `${this._name} is ${status}.`;
+        return result;
+    }
+
+    get name() { return this._name; }
+    get start() { return this._running; }
+    set start(value) { this._running = value; }
+}
+
+let myCar = new Car('my toyota');
+console.log(myCar.status);
+myCar.start = true;
+console.log(myCar.status);
+
+const Brake = function() {
+    this.start = false;
+}
+
+Object.assign(Car.prototype, { Brake });
+myCar.Brake();
+console.log(myCar.status);
