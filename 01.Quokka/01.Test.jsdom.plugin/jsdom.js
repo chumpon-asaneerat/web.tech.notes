6abec3cbda49692d7f0f574360b32b038d5c5ae1
chumpon-asaneerat <chumponsenate@gmail.com>
2019-12-20 17:39:56 +0700

    Update quoakka sample and install local packages.

--- 01.Quokka/01.Test.jsdom.plugin/jsdom.js
+++ 01.Quokka/01.Test.jsdom.plugin/jsdom.js
@@ -1,3 +1,17 @@
+//#region Setup
+
+({ "plugins": [ "jsdom-quokka-plugin", "quokka-jquery-loader" ] })
+
+let filename = 'index.html' // <<== Setup your file here
+
+let path = require('path')
+let fs = require('fs')
+let __html = path.join(__dirname, filename)
+let __root = document.getElementsByTagName('html')[0];
+__root.innerHTML = fs.readFileSync(__html, { encoding: 'UTF8'});
+
+//#endregion
+
 console.log(document.getElementById('app').innerHTML)
 
 var app = $('#app')
